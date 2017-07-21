22c09739f6f3b54ad6706b9650a3eb388c153ab9
Diego Dantas <diegodant@gmail.com>
2017-07-21 09:40:50 -0300

    aula 54
    Evento onChange

--- ProjetoUmReact/frontend/src/todo/todo.jsx
+++ ProjetoUmReact/frontend/src/todo/todo.jsx
@@ -1,14 +1,39 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageheader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={description:'', list: [] }
+        
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        const description = this.state.description
+        axios.post(URL,{description})
+        .then(resp => console.log('Funcionou! '))
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description:e.target.value})
+    }
+    
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas ' small='Cadastro'></PageHeader>
-                <TodoForm/>
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange} 
+                    handleAdd={this.handleAdd}/>
                 <TodoList/>
             </div>
 
--- ProjetoUmReact/frontend/src/todo/todoForm.jsx
+++ ProjetoUmReact/frontend/src/todo/todoForm.jsx
@@ -3,14 +3,18 @@ import Grid from '../template/grid'
 import IconButton from '../template/iconbutton'
 
 export default props => (
+
     <div role='form' className='todoForm'>
         
         <Grid cols= '12 9 10'>
-            <input id ='description' className='form-control'placeholder='Adicione uma tarefa'></input>
+            <input id ='description' className='form-control'
+            value={props.description} placeholder='Adicione uma tarefa'
+            onChange={props.handleChange}></input>
         </Grid>
         
          <Grid cols= '12 3 2'>
-             <IconButton style='primary' icon='plus'></IconButton>
+             <IconButton style='primary' icon='plus'
+             onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
